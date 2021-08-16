f23ee9ff7f6d33438c3f550baf799206699729e1
tenclar <tenclarvalus@gmail.com>
2021-08-16 01:00:13 -0500

    lista de posts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,36 +1,96 @@
 import { GetStaticProps } from 'next';
-
+import { FiCalendar, FiUser } from 'react-icons/fi';
+import Head from 'next/head';
+import Prismic from '@prismicio/client';
+import { RichText } from 'prismic-dom';
+import Link from 'next/link';
+import { format } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
 import { getPrismicClient } from '../services/prismic';
-
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
 
 interface Post {
-  uid?: string;
   first_publication_date: string | null;
-  data: {
-    title: string;
-    subtitle: string;
-    author: string;
-  };
+  slug: string;
+  title: string;
+  subtitle: string;
+  author: string;
 }
 
 interface PostPagination {
   next_page: string;
   results: Post[];
+  posts: Post[];
 }
 
 interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home({ posts }: PostPagination) {
+  return (
+    <>
+      <Head>
+        <title> SpaceTraveling. </title>
+      </Head>
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient();
-//   // const postsResponse = await prismic.query(TODO);
+      <main className={commonStyles.container}>
+        <div className={styles.postList}>
+          {posts.map(post => (
+            <Link href={`/post/${post.slug}`}>
+              <a key={post.slug}>
+                <strong>{post.title}</strong>
+                <sub>{post.subtitle}</sub>
+                <div className={styles.info}>
+                  <time>
+                    <FiCalendar size={20} />
+                    {post.first_publication_date}
+                  </time>
+                  <span>
+                    <FiUser size={20} />
+                    {post.author}
+                  </span>
+                </div>
+              </a>
+            </Link>
+          ))}
+          <a className={styles.buttonPosts} type="button">
+            Carregar Mais Posts
+          </a>
+        </div>
+      </main>
+    </>
+  );
+}
 
-//   // TODO
-// };
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+  const postsResponse = await prismic.query(
+    [Prismic.predicates.at('document.type', 'posts')],
+    {
+      fetch: ['posts.title', 'posts.subtitle', 'posts.author'],
+      pageSize: 100,
+    }
+  );
+  const posts = postsResponse.results.map(post => {
+    return {
+      slug: post.uid,
+      title: RichText.asText(post.data.title),
+      subtitle: RichText.asText(post.data.subtitle),
+      author: RichText.asText(post.data.author),
+      first_publication_date: format(
+        new Date(post.first_publication_date),
+        'dd MMM yyyy',
+        {
+          locale: ptBR,
+        }
+      ),
+    };
+  });
+
+  // console.log(JSON.stringify(postsResponse, null, 2));
+  return {
+    props: { posts },
+  };
+};
--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -1,11 +1,16 @@
 import { GetStaticPaths, GetStaticProps } from 'next';
-
+import Head from 'next/head';
+import Prismic from '@prismicio/client';
+import { format } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
+import { RichText } from 'prismic-dom';
 import { getPrismicClient } from '../../services/prismic';
 
 import commonStyles from '../../styles/common.module.scss';
 import styles from './post.module.scss';
 
 interface Post {
+  slug: string;
   first_publication_date: string | null;
   data: {
     title: string;
@@ -26,20 +31,88 @@ interface PostProps {
   post: Post;
 }
 
-// export default function Post() {
-//   // TODO
-// }
+export default function Post({ post }: PostProps) {
+  return (
+    <>
+      <Head>
+        <title> SpaceTraveling. - Posts </title>
+      </Head>
+      <div className={commonStyles.container}>
+        <h1 className={styles.title}>Posts</h1>
+      </div>
+    </>
+  );
+}
 
-// export const getStaticPaths = async () => {
-//   const prismic = getPrismicClient();
-//   const posts = await prismic.query(TODO);
+export const getStaticPaths: GetStaticPaths = async () => {
+  const prismic = getPrismicClient();
+  const posts = await prismic.query([
+    Prismic.predicates.at('document.type', 'posts'),
+  ]);
+  const paths = posts.results.map(post => ({ params: { slug: post.uid } }));
 
-//   // TODO
-// };
+  return {
+    paths,
+    fallback: true,
+  };
+};
 
-// export const getStaticProps = async context => {
-//   const prismic = getPrismicClient();
-//   const response = await prismic.getByUID(TODO);
+export const getStaticProps: GetStaticProps = async ({
+  params,
+  preview = false,
+  previewData,
+}) => {
+  const { slug } = params;
+  const prismic = getPrismicClient();
 
-//   // TODO
-// };
+  const response = await prismic.getByUID('posts', String(slug), {
+    ref: previewData?.ref ?? null,
+  });
+
+  const prevPost = await prismic.query(
+    [Prismic.predicates.at('document.type', 'post')],
+    {
+      pageSize: 1,
+      after: response.id,
+      orderings: '[document.first_publication_date]',
+    }
+  );
+
+  const nextPost = await prismic.query(
+    [Prismic.predicates.at('document.type', 'post')],
+    {
+      pageSize: 1,
+      after: response.id,
+      orderings: '[document.last_publication_date]',
+    }
+  );
+
+  if (!response) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      },
+    };
+  }
+
+  const post = {
+    slug,
+    first_publication_date: format(
+      new Date(response.first_publication_date),
+      'dd MMM yyyy',
+      { locale: ptBR }
+    ),
+    data: {
+      title: RichText.asText(response.data.title),
+      subtitle: RichText.asText(response.data.subtitle),
+      banner: response.data.banner,
+      author: RichText.asText(response.data.author),
+      content: response.data.content,
+    },
+  };
+
+  return {
+    props: { post, preview },
+  };
+};
